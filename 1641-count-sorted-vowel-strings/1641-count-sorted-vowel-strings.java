c25bb6f23e68ea06fa93a1f9d66d2ca3f9e2e27e
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-11 10:26:45 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (19.45%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,5 +1,14 @@
 class Solution {
     public int countVowelStrings(int n) {
-        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+        // return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+        int a = 1, e = 1, i = 1, o = 1;
+        for(int j = 2; j <= n; j++){
+            a = a + e + i + o + 1;
+            e = e + i + o + 1;
+            i = i + o + 1;
+            o = o + 1;
+            // u = u;
+        }
+        return a + e + i + o + 1;
     }
 }
