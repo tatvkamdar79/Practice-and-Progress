1c7a0980cba519ee7e342a87ad80c8b6471f6508
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-12 11:23:50 +0530

    Time: 190 ms (20.53%), Space: 97.7 MB (21.57%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int carFleet(int target, int[] pos, int[] speed) {
+        Map<Integer, Double> m = new TreeMap<>(Collections.reverseOrder());
+        
+        for(int i = 0; i < pos.length; ++i)     m.put(pos[i], (double)(target - pos[i]) / speed[i]);
+        
+        int res = 0; double cur = 0;
+        
+        for (double time : m.values()){
+            if (time > cur){
+                cur = time;
+                res++;
+            }
+        }
+        return res;
+    }
+}
