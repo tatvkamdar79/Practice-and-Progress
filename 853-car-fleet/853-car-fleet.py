c39426775a0d4fbfd6524e2fe342b7a65cc3788d
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-12 11:20:55 +0530

    Time: 972 ms (89.36%), Space: 38.2 MB (9.33%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,12 +1,19 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = prev = 0
-        for pp, ss in sorted(zip(position, speed), reverse=True): 
-            tt = (target - pp)/ss # time to arrive at target 
-            if prev < tt: 
-                ans += 1
-                prev = tt
-        return ans
+        arrival_time = {p: (target - p) / s for p,s in zip(position, speed)}
+        position.sort(reverse=True)
+        
+        i = 0
+        fleets = 0
+        while i < len(position):
+            # ith closest car has arrived 
+            # start new fleet
+            fleets += 1
+            fleet_arrival = arrival_time[position[i]]
+            while i < len(position) and arrival_time[position[i]] <= fleet_arrival:
+                i += 1
+        
+        return fleets
         # For Understanding
         # arrivals = [(target-position[i])/speed[i] for i in range(len(position))]
         # t = sorted(zip(position, speed, arrivals), key = lambda x: x[0])
