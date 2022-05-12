0bcef7bae14ffa01346af672c7dc41e7ec9de2b9
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-12 11:20:07 +0530

    Time: 1599 ms (20.82%), Space: 36.4 MB (45.88%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,17 +1,25 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        arrivals = [(target-position[i])/speed[i] for i in range(len(position))]
-        t = sorted(zip(position, speed, arrivals), key = lambda x: x[0])
-        # print(t)
-        curr = t[-1]
-        st = []
-        st.append(curr)
-        # res = 0
-        for i in range(len(t) - 2, -1, -1):
-            if st != [] and st[-1][2] >= t[i][2]:
-                continue
-            else:
-                st.append(t[i])
-                curr = t[i]
-            # print(st)
-        return len(st)
+        ans = prev = 0
+        for pp, ss in sorted(zip(position, speed), reverse=True): 
+            tt = (target - pp)/ss # time to arrive at target 
+            if prev < tt: 
+                ans += 1
+                prev = tt
+        return ans
+        # For Understanding
+        # arrivals = [(target-position[i])/speed[i] for i in range(len(position))]
+        # t = sorted(zip(position, speed, arrivals), key = lambda x: x[0])
+        # # print(t)
+        # curr = t[-1]
+        # st = []
+        # st.append(curr)
+        # # res = 0
+        # for i in range(len(t) - 2, -1, -1):
+        #     if st != [] and st[-1][2] >= t[i][2]:
+        #         continue
+        #     else:
+        #         st.append(t[i])
+        #         curr = t[i]
+        #     # print(st)
+        # return len(st)
