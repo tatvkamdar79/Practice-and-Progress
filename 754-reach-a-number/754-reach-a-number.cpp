cda31a5c3c3ac0e97b903eba7c488d17e3179ce9
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-14 11:36:05 +0530

    Time: 6 ms (11.39%), Space: 5.9 MB (69.31%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.cpp
+++ 754-reach-a-number/754-reach-a-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int reachNumber(int target) {
+        // O(1) Solution
+        
         // target = abs(target);
         // long long n = ceil((-1 + sqrt(1 + 8.0*(target))) / 2);
         // long long sum = (n * (n+1)) / 2;
@@ -8,6 +10,10 @@ public:
         // long long rem = sum - target;
         // if((rem&1) == 0)    return n;
         // return n + ((n&1) ? 2 : 1);
+        
+        
+        // O(logn) ig
+        
         int sum = 0, steps = 0;
         target = abs(target);
         while(sum < target || (target - sum) % 2 != 0) {
