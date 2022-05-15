820ef62aafb8ca860b092739f1f4b51103d6d497
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-15 15:58:05 +0530

    Time: 55 ms (67.48%), Space: 13.9 MB (57.06%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.py
+++ 754-reach-a-number/754-reach-a-number.py
@@ -1,26 +1,9 @@
-class Solution {
-    public int reachNumber(int target) {
-        // int steps = 1;
-        // int soFar = 0;
-        // target = Math.abs(target);
-        // while(soFar < target){
-        //     soFar += steps++;
-        // }
-        // if(soFar == target) return steps-1;
-        // int diff = soFar - target;
-        // while((soFar - target) % 2 != 0){
-        //     soFar += steps++;
-        // }
-        // return steps-1;
-
-        target = Math.abs(target);
-        int n = (int)Math.ceil((-1 + Math.sqrt(1 + 8.0*(target))) / 2);
-        int sum = (n * (n+1)) / 2;
-        if(sum == target)   return n;
-        int rem = sum - target;
-        if((rem&1) == 0)    return n;
-        if((n & 1) == 1) return n + 2;
-        return n + 1;
-        // return n + (int)((n & 1) ? 2 : 1);
-    }
-}
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        target = abs(target)
+        n = int(math.ceil((math.sqrt(8 * target + 1) - 1) / 2)) # solve inequation: n * (n + 1) / 2 >= target
+        d = n * (1 + n) // 2 - target
+        if d & 1 == 0:
+            return n
+        else:
+            return n + (0 if n&1 == 0 else 1) + 1
