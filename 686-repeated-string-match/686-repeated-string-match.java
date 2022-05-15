67d8229dc5e6e7eabc4715e07fd56e6b5017f719
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-15 16:15:24 +0530

    Time: 331 ms (35.18%), Space: 42.2 MB (78.31%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(a);
+        int count = 1;
+        while (sb.indexOf(b) == -1) {
+            if (sb.length() - a.length() > b.length()) {
+                return -1;
+            }
+            sb.append(a);
+            count++;
+        }
+        return count;
+    }
+}
