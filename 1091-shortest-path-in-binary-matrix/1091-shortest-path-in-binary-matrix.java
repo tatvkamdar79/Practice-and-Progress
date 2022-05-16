306930121c443d3e16ebaa026fb2aa79633e3e7d
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-16 12:40:43 +0530

    Time: 7 ms (99.93%), Space: 43.7 MB (86.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,42 +1,58 @@
-class Solution{
-private int dir[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1},{-1,-1},{1,1}};
-
-    public int shortestPathBinaryMatrix(int[][] grid) {
-
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(grid[0][0]==1 || grid[m-1][n-1]==1) {
-            return -1;
-        }
-
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0] = true;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-
-        int ans=0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i=0;i<size;i++) {
-                int[] pop = queue.remove();
-                if(pop[0]==m-1 && pop[1]==n-1) {
-                    return ans+1;
-                }
-                for (int k=0;k<8;k++) {
-                    int nextX = dir[k][0]+pop[0];
-                    int nextY = dir[k][1]+pop[1];
-
-                    if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && !visited[nextX][nextY] && grid[nextX][nextY]==0) {
-                        queue.add(new int[]{nextX,nextY});
-                        visited[nextX][nextY]=true;
-                    }
-
-                }
+class Solution {
+   public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if(grid[0][0]==1||grid[n-1][n-1]==1)return -1;
+        if(n==1&&grid[0][0]==0)return 1;
+        grid[0][0]=1;
+        List<int[]> pos=new ArrayList<>();
+        pos.add(new int[]{0,0});
+        check(grid,pos);
+        if(grid[n-1][n-1]==0)return -1;
+        return grid[n-1][n-1];
+        
+    }
+    public void check(int[][] grid,List<int[]> pos){
+        int n=grid.length;
+        List<int[]> newpos=new ArrayList<>();
+        for(int[] p:pos){
+            int i=p[0];
+            int j=p[1];
+            int curdis=grid[i][j]+1;
+            if(i>0&&j>0&&grid[i-1][j-1]==0){
+                grid[i-1][j-1]=curdis;
+                newpos.add(new int[]{i-1,j-1});
             }
-            ans++;
+            if(i<n-1&&j>0&&grid[i+1][j-1]==0){
+                grid[i+1][j-1]=curdis;
+                newpos.add(new int[]{i+1,j-1});
+            }
+            if(i>0&&j<n-1&&grid[i-1][j+1]==0){
+                grid[i-1][j+1]=curdis;
+                newpos.add(new int[]{i-1,j+1});
+            }
+            if(i<n-1&&j<n-1&&grid[i+1][j+1]==0){
+                grid[i+1][j+1]=curdis;
+                newpos.add(new int[]{i+1,j+1});
+            }
+            if(j>0&&grid[i][j-1]==0){
+                grid[i][j-1]=curdis;
+                newpos.add(new int[]{i,j-1});
+            }
+            if(j<n-1&&grid[i][j+1]==0){
+                grid[i][j+1]=curdis;
+                newpos.add(new int[]{i,j+1});
+            }
+            if(i<n-1&&grid[i+1][j]==0){
+                grid[i+1][j]=curdis;
+                newpos.add(new int[]{i+1,j});
+            }
+            if(i>0&&grid[i-1][j]==0){
+                grid[i-1][j]=curdis;
+                newpos.add(new int[]{i-1,j});
+            }
         }
-
-        return -1;
+        if(!newpos.isEmpty())check(grid,newpos);
+        
+        
     }
 }
