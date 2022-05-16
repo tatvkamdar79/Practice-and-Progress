874f858159d399908c49ff3c6d82dd788772120b
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-16 19:58:39 +0530

    Time: 1 ms (32.41%), Space: 42.9 MB (34.51%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int target) {
+//         if(mat == null || mat.length == 0 || mat[0].length == 0)    return false;
+//         if(mat.length == 1) return binarySearch(mat, 0, target);
+//         if(mat.length == 1 && mat[0].length == 1)   return mat[0][0] == target;
+//         int n = mat[0].length - 1;
+//         int start = 0, end = mat.length - 1;
+//         while(start < end) {
+//             int mid = (start + end)/2;
+//             if(mat[mid][n] == target)   return true;
+//             else if(mat[mid][n] > target)   end = mid;
+//             else    start = mid + 1;
+//         }
+        
+//         return binarySearch(mat, start, target) || binarySearch(mat, end, target);
+//     }
+    
+//     public boolean binarySearch(int[][] mat, int row, int target) {
+//         int s = 0, e = mat[row].length;
+//         while(s < e) {
+//             int mid = (s+ e)/2;
+//             if(mat[row][mid] == target)   return true;
+//             if(mat[row][mid] > target)  e = mid;
+//             else    s = mid + 1;
+//         }
+//         return false;
+//     }
+        int r = 0, c = mat[0].length-1;
+        
+        while(r<mat.length && c>=0){
+            if(mat[r][c]==target)    return true;
+            else if(mat[r][c]<target)    r++;
+            else    c--;
+        }
+        return false;
+    }
+}
