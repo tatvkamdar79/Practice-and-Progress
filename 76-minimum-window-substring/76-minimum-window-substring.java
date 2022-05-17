ffeaf22c9c8f68d14f4c11fcdead3fcec2e73675
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-17 13:10:34 +0530

    Time: 3 ms (96.60%), Space: 44 MB (61.40%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,46 +1,26 @@
-public class Solution {
-public String minWindow(String s, String t) {
-    if(s == null || s.length() < t.length() || s.length() == 0){
-        return "";
-    }
-    HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-    for(char c : t.toCharArray()){
-        if(map.containsKey(c)){
-            map.put(c,map.get(c)+1);
-        }else{
-            map.put(c,1);
-        }
-    }
-    int left = 0;
-    int minLeft = 0;
-    int minLen = s.length()+1;
-    int count = 0;
-    for(int right = 0; right < s.length(); right++){
-        if(map.containsKey(s.charAt(right))){
-            map.put(s.charAt(right),map.get(s.charAt(right))-1);
-            if(map.get(s.charAt(right)) >= 0){
-                count ++;
-            }
-            while(count == t.length()){
-                if(right-left+1 < minLen){
-                    minLeft = left;
-                    minLen = right-left+1;
-                }
-                if(map.containsKey(s.charAt(left))){
-                    map.put(s.charAt(left),map.get(s.charAt(left))+1);
-                    if(map.get(s.charAt(left)) > 0){
-                        count --;
-                    }
-                }
-                left ++ ;
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] need = new int[123];
+        int miss = t.length();
+        char[] have = s.toCharArray();
+        for(char c : t.toCharArray()) need[c]++;
+        int i = 0, j = 0, l = 0, r = 0;
+        
+        while(r < s.length()){
+            if(need[have[r]] > 0) miss--;
+            need[have[r]]--;
+            r++;
+            
+            while(miss == 0){
+                if(j == 0 || (r-l) < (j-i)){
+                    j = r;
+                    i = l;
+                } 
+                need[have[l]]++;
+                if(need[have[l]] > 0)miss++;
+                l++;
             }
         }
+        return s.substring(i,j);
     }
-    if(minLen>s.length())  
-    {  
-        return "";  
-    }  
-    
-    return s.substring(minLeft,minLeft+minLen);
-}
 }
