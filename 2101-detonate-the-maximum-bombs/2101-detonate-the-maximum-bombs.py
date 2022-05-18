21cd638083ee51266bc15bc84b98f015baff9daf
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-18 19:29:00 +0530

    Time: 429 ms (96.51%), Space: 14.3 MB (72.05%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,23 +1,38 @@
 class Solution:
-    def maximumDetonation(self, B: List[List[int]]) -> int:
-        n, ans, G = len(B), 0, defaultdict(list)
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        import math
+        from collections import deque
+        self.graph = {i: [] for i in range(len(bombs))}
         
-        for i in range(n):
-            for j in range(n):
-                if i == j: continue
-                if B[i][2]**2 >= (B[i][0] - B[j][0])**2 + (B[i][1] - B[j][1])**2:
-                    G[i] += [j]
+        # builing graph takes ~ O(2N) where N is # of bombs
+        for i in range(len(bombs)):
+            bomb1 = bombs[i]
+            for j in range(i+1, len(bombs)):
+                bomb2 = bombs[j]
+                dist_x = bomb1[0]-bomb2[0]
+                dist_y = bomb1[1]-bomb2[1]
+                distance = math.sqrt((dist_x*dist_x) + (dist_y*dist_y))
+                if distance <= bomb1[2]:
+                    self.graph[i].append(j)
+                if distance <= bomb2[2]:
+                    self.graph[j].append(i)
         
+        # Looping through all nodes takes O(N) and dfs takes O(N) in w.c.s
+        # Hence, total T.C is O(N^2)
+        max_explosions = 1            
+        for i in range(len(bombs)):
+            seen_nodes = set([i])
+            explosion_count = self.get_explosion_count(i, seen_nodes)
+            max_explosions = max(max_explosions, explosion_count)
+            if max_explosions == len(bombs):
+                break
+        return max_explosions
+    
+    def get_explosion_count(self, curr_node, seen_nodes):
+        explosion_count = 0
+        for child_node in self.graph[curr_node]:
+            if child_node not in seen_nodes:
+                seen_nodes.add(child_node)
+                explosion_count += self.get_explosion_count(child_node, seen_nodes)
+        return explosion_count+1
         
-        def dfs(node, visited):
-            for child in G[node]:
-                if child not in visited:
-                    visited.add(child)
-                    dfs(child, visited)
-
-        for i in range(n):
-            visited = set([i])
-            dfs(i, visited)
-            ans = max(ans, len(visited))
-                          
-        return ans
