2b49128405727e915078f31f961e18e5ff29f7ec
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-21 22:23:37 +0530

    Time: 2738 ms (16.38%), Space: 14 MB (95.60%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,5 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        
         dp = [amount + 1] * (amount + 1)
         
         # print(dp)
@@ -10,5 +12,7 @@ class Solution:
             for c in coins:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a-c])
+                else:
+                    break
         # print(dp)
         return dp[amount] if dp[amount] != amount + 1 else -1
