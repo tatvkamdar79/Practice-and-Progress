9da7990b3d20149c03f696a8efc50297a11518b8
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-23 21:11:30 +0530

    Time: 2 ms (30.47%), Space: 48.2 MB (29.69%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,37 +1,52 @@
 class Solution {
-    public int[][] floodFill(int[][] img, int sr, int sc, int newColor) {
-        Queue<List<Integer>> q = new LinkedList<>();
-        Set<List<Integer>> s = new HashSet<>();
-        
-        q.add(Arrays.asList(sr,sc));
-        s.add(Arrays.asList(sr,sc));
-        
-        int r_bound = img.length, c_bound = img[0].length, startPixel = img[sr][sc];
-        
-        img[sr][sc] = newColor;
-        
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image==null || image.length==0 || image[sr][sc]==newColor){
+            return image;
+        }
+        int color = image[sr][sc];
+        image[sr][sc] = newColor;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        q.add(new int[]{sr, sc});
         while(!q.isEmpty()){
-            List<Integer> l = q.remove();
-            
-            int r = l.get(0), c = l.get(1);
-            
-            if(r+1 < r_bound && s.add(Arrays.asList(r+1,c)) && img[r+1][c] == startPixel){
-                q.add(Arrays.asList(r+1,c));
-                img[r+1][c] = newColor;
-            }
-            if(r-1 >= 0 && s.add(Arrays.asList(r-1,c)) && img[r-1][c] ==  startPixel){
-                q.add(Arrays.asList(r-1,c));
-                img[r-1][c] = newColor;
-            }
-            if(c+1 < c_bound && s.add(Arrays.asList(r,c+1)) && img[r][c+1] == startPixel){
-                q.add(Arrays.asList(r,c+1));
-                img[r][c+1] = newColor;
-            }
-            if(c-1 >= 0 && s.add(Arrays.asList(r,c-1)) && img[r][c-1] == startPixel){
-                q.add(Arrays.asList(r,c-1));
-                img[r][c-1] = newColor;
+            int size = q.size();
+            while(!q.isEmpty()){
+                int[] curr = q.poll();
+                for(int[] dir : dirs){
+                    int r = dir[0] + curr[0];
+                    int c = dir[1] + curr[1];
+                    if(r>=0 && r<image.length && c>=0 && c<image[r].length && image[r][c] == color){
+                        image[r][c] = newColor;
+                        q.add(new int[]{r, c});
+                    }
+                }
             }
         }
-        return img;
+        return image;
     }
+    // D F S
+    
+    // static void dfs(int [][]image,int sr,int sc,int newColor,int row,int col,int src ){
+    //     if(sr<0||sr>=row||sc<0||sc>=col){
+    //         return ;
+    //     }
+    //     else if(image[sr][sc]!=src){
+    //         return ;
+    //     }
+    //     image[sr][sc]=newColor;
+    //     dfs(image,sr-1,sc,newColor,row,col,src);
+    //     dfs(image,sr+1,sc,newColor,row,col,src);
+    //     dfs(image,sr,sc-1,newColor,row,col,src);
+    //     dfs(image,sr,sc+1,newColor,row,col,src);
+    // }
+    // public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    //     if(newColor==image[sr][sc]){
+    //         return image;
+    //     }
+    //     int row=image.length;
+    //     int col=image[0].length;
+    //     int src=image[sr][sc];
+    //     dfs(image,sr,sc,newColor,row,col,src);
+    //  return image;   
+    // }
 }
