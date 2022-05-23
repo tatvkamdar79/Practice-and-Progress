6056ab0bf855d50daf69301efd3bd2d08d5d3503
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-23 11:37:18 +0530

    Time: 40 ms (77.78%), Space: 89.3 MB (77.78%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -1,20 +1,21 @@
 class Solution {
     public int minimumLines(int[][] stockPrices) {
-        if (stockPrices.length == 1) return 0;
-        // Sort by occurence of each day and then check the slope
-        // Time: O(NLogN), Space: O(1) / O(N) which depends on the type of sort used
-        Arrays.sort(stockPrices, (a, b) -> Integer.compare(a[0], b[0]));
-        int count = 1;
-        for (int i = 2; i < stockPrices.length; i++) {
-            // Check if the slopes of three consecutive points are equal then continue
-            // otherwise add another line to the count.
-            // check (y2 - y1) / (x2 - x1) == (y1 - y0) / (x1 - x0) => (y2 - y1) * (x1 - x0) == (y1 - y0) * (x2 - x1)
-            if ((stockPrices[i][1] - stockPrices[i - 1][1]) * (stockPrices[i - 1][0] - stockPrices[i - 2][0]) == 
-                (stockPrices[i - 1][1] - stockPrices[i - 2][1]) * (stockPrices[i][0] - stockPrices[i - 1][0])) continue;
+        Arrays.sort(stockPrices, (a, b) -> a[0] - b[0]);
+        
+        int result = Math.min(stockPrices.length - 1, 1);
+        long n1, n2, d1, d2;
+        
+        for (int i = 2; i < stockPrices.length; ++i ){
+            n1 = stockPrices[i][1] - stockPrices[i - 1][1];
+            d1 = stockPrices[i][0] - stockPrices[i - 1][0];
             
-            count += 1;
+            n2 = stockPrices[i - 1][1] - stockPrices[i - 2][1];
+            d2 = stockPrices[i - 1][0] - stockPrices[i - 2][0];
+            
+            if (n1*d2 != n2*d1)
+                ++result;
         }
-
-        return count;
+        
+        return result;
     }
 }
