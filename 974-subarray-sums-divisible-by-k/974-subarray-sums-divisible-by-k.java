97bf9dab0543404ab01db42cf5de3283aad34f95
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-24 22:59:20 +0530

    Time: 5 ms (93.40%), Space: 54.9 MB (64.12%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -1,16 +1,27 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int sum = 0, rem = 0, count = 0;
-        Map<Integer, Integer> mp = new HashMap<>();
-        mp.put(0,1);
-        for(int i = 0; i < nums.length; i++){
+        // int sum = 0, rem = 0, count = 0;
+        // Map<Integer, Integer> mp = new HashMap<>();
+        // mp.put(0,1);
+        // for(int i = 0; i < nums.length; i++){
+        //     sum += nums[i];
+        //     rem = sum % k;
+        //     if(rem < 0) rem += k;
+        //     if(mp.containsKey(rem)){
+        //         count += mp.get(rem);
+        //     }
+        //     mp.put(rem, mp.getOrDefault(rem, 0) + 1);
+        // }
+        // return count;
+        int[] frq = new int[k];
+        int sum=0, count =0;
+        frq[0] = 1;
+        for(int i=0 ; i<nums.length ; i++){
             sum += nums[i];
-            rem = sum % k;
+            int rem = sum%k;
             if(rem < 0) rem += k;
-            if(mp.containsKey(rem)){
-                count += mp.get(rem);
-            }
-            mp.put(rem, mp.getOrDefault(rem, 0) + 1);
+            count += frq[rem];
+            frq[rem]++;
         }
         return count;
     }
