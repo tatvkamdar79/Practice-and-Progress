659560e6bbf8a67e8cb6c7e04955e7edf6c92fdb
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-25 22:45:34 +0530

    Time: 76 ms (95.68%), Space: 14.2 MB (84.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,17 +1,23 @@
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        int ans = 1;
-        for(int i = 0; i < nums.length; i++){
-            int longest_so_far = 0;
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i])
-                    longest_so_far = Math.max(longest_so_far, dp[j]);
-            }
-            dp[i] = 1 + longest_so_far;
-            ans = Math.max(ans, dp[i]);
-        }
-        return ans;
-    }
-}
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)
+                sub[idx] = x
+        
+        return len(sub)
+                
+#         dp = [1]*len(nums)
+        
+#         for i in range(len(nums)):
+#             m = 0
+#             for j in range(i):
+#                 if nums[j] < nums[i]:
+#                     m = max(m, dp[j])
+#             dp[i] = 1 + m
+#         # print(nums)
+#         # print(dp)
+#         return max(dp)
