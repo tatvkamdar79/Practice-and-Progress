92d05c2fe603d6203dea9b736bf642bdcec537ab
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-25 23:11:49 +0530

    Time: 3 ms (97.65%), Space: 44.8 MB (27.99%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,30 @@
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        sub = []
-        for x in nums:
-            if len(sub) == 0 or sub[-1] < x:
-                sub.append(x)
-            else:
-                idx = bisect_left(sub, x)
-                sub[idx] = x
+class Solution {    
+    public int lengthOfLIS(int[] nums) {
+        int[] sub = new int[nums.length];
+        int len = 0;
         
-        return len(sub)
-                
-#         dp = [1]*len(nums)
-        
-#         for i in range(len(nums)):
-#             m = 0
-#             for j in range(i):
-#                 if nums[j] < nums[i]:
-#                     m = max(m, dp[j])
-#             dp[i] = 1 + m
-#         # print(nums)
-#         # print(dp)
-#         return max(dp)
+        for(int x : nums){
+            int idx = Arrays.binarySearch(sub, 0, len, x);
+            if(idx < 0) idx = -(idx + 1);
+            sub[idx] = x;
+            if(idx == len)    len++;
+        }
+        return len;
+    }
+}
+    //     // O ( N ^ 2 )
+    //     int[] dp = new int[nums.length];
+    //     Arrays.fill(dp, 1);
+    //     int ans = 1;
+    //     for(int i = 0; i < nums.length; i++){
+    //         int longest_so_far = 0;
+    //         for(int j = 0; j < i; j++){
+    //             if(nums[j] < nums[i])
+    //                 longest_so_far = Math.max(longest_so_far, dp[j]);
+    //         }
+    //         dp[i] = 1 + longest_so_far;
+    //         ans = Math.max(ans, dp[i]);
+    //     }
+    //     return ans;
+    // }
+// }
