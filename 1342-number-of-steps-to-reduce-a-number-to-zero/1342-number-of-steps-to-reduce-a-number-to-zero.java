9f36d5f6562fd504a3c34f7d0bb5e1d627e9002f
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-27 14:12:09 +0530

    Time: 1 ms (54.47%), Space: 41.5 MB (18.39%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,9 +1,16 @@
 class Solution {
     public int numberOfSteps(int num) {
-        String s = Integer.toBinaryString(num);
-        int zeros = (int)s.chars().filter(a -> a == '0').count();
-        int ones = s.length() - zeros;
-        // System.out.println(ones);
-        return ones*2 + zeros - 1;
+        int count = 1;
+        if(num == 0){return 0;}
+        if( num / 2== 0 || num - 1 == 0){return count;}
+        
+        if(num % 2 == 0){ return count + numberOfSteps(num/2);}
+        
+        return count + numberOfSteps(num-1);
+        // String s = Integer.toBinaryString(num);
+        // int zeros = (int)s.chars().filter(a -> a == '0').count();
+        // int ones = s.length() - zeros;
+        // // System.out.println(ones);
+        // return ones*2 + zeros - 1;
     }
 }
