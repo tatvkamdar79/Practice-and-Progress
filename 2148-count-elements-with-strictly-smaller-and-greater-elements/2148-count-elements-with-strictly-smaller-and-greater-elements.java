57190df59b8ff65609c545822c7fc2b9b1d494c3
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-30 01:59:39 +0530

    Time: 1 ms (86.09%), Space: 43.1 MB (6.15%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,18 +1,28 @@
 class Solution {
     public int countElements(int[] nums) {
-        Arrays.sort(nums);
-        int count = 0;
-        for(int i = 1; i < nums.length - 1; i++){
-            int l = i - 1, r = i + 1;
-            while(l > -1 && r < nums.length){
-                if(nums[l] < nums[i] && nums[r] > nums[i]){
-                    count++;
-                    break;
-                }
-                if(nums[l] == nums[i])  l--;
-                if(nums[r] == nums[i])  r++;
-            }
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
         }
+        int count = 0;
+        for(int i = 0; i < nums.length; i++)
+            if(nums[i] < max && nums[i] > min)
+                count++;
         return count;
+        // Arrays.sort(nums);
+        // int count = 0;
+        // for(int i = 1; i < nums.length - 1; i++){
+        //     int l = i - 1, r = i + 1;
+        //     while(l > -1 && r < nums.length){
+        //         if(nums[l] < nums[i] && nums[r] > nums[i]){
+        //             count++;
+        //             break;
+        //         }
+        //         if(nums[l] == nums[i])  l--;
+        //         if(nums[r] == nums[i])  r++;
+        //     }
+        // }
+        // return count;
     }
 }
