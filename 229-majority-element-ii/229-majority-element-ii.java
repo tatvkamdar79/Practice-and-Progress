9ace610d44b001865ed14a983621720b70e4903e
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-05-30 02:25:24 +0530

    Time: 3 ms (74.97%), Space: 49.7 MB (52.09%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,13 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
-        Set<Integer> s = new HashSet<>();
-        int req = nums.length / 3;
-        for(int i = 0; i < nums.length; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
-            if(mp.get(nums[i]) > req && s.add(nums[i]))   res.add(nums[i]);
+        if(nums.length == 1)    return Arrays.asList(nums[0]);
+        
+        int num1 = nums[0], num2 = nums[1], c1 = 0, c2 = 0;
+        for(int i : nums){
+            if(i == num1)
+                c1++;
+            else if(i == num2)
+                c2++;
+            else if(c1 == 0){
+                num1 = i; c1 = 1;
+            }
+            else if(c2 == 0){
+                num2 = i; c2 = 1;
+            }
+            else{
+                c1--; c2--;
+            }
         }
+        c1 = 0; c2 = 0;
+        for(int i : nums){
+            if(i == num1)   c1++;
+            else if(i == num2)   c2++;
+        }
+        List<Integer> res = new ArrayList<>();
+        if(c1 > nums.length / 3)    res.add(num1);
+        if(c2 > nums.length / 3)    res.add(num2);
+
         return res;
     }
 }
