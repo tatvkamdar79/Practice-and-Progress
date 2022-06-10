c4707c1c4d695ab4887da7c0a011ee08d38d2a0a
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-06-10 21:33:26 +0530

    Time: 52 ms (39.51%), Space: 116.4 MB (50.43%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,6 +1,11 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0]*b[0] + b[1]*b[1] - a[0]*a[0] - a[1]*a[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() { 
+            @Override
+            public int compare(int[] left, int[] right) {
+                return getDistance(right) - getDistance(left);
+            }
+        });
         for(int[] p : points){
             pq.offer(p);
             if(pq.size() > k)
@@ -11,5 +16,8 @@ class Solution {
             res[--k] = pq.poll();
         }
         return res;
+    }
+    private int getDistance(int [] point) {
+        return point[0] * point[0] + point[1] * point[1];
     }
 }
