6b4c03a974fd88c36bc4e666951db8845f778c70
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-06-13 13:59:01 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (39.36%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -13,21 +13,45 @@
  *     }
  * }
  */
-class Solution {
-    Set<String> l = new HashSet<>();
+class Solution{
     public List<String> binaryTreePaths(TreeNode root) {
-        rec(root, "");
-        return new LinkedList<String>(l); 
-    }
-    public void rec(TreeNode root, String s){
-        if(root == null)
-            return;
-        if(root.left == null && root.right == null){
-            l.add(s+Integer.toString(root.val));
+            List<String> res = new ArrayList<>();
+            StringBuilder sb = new StringBuilder();
+            helper(res, root, sb);
+            return res;
+        }
+
+    private void helper(List<String> res, TreeNode root, StringBuilder sb) {
+        if(root == null) {
             return;
         }
-        s += Integer.toString(root.val) + "->";
-        rec(root.left, s);
-        rec(root.right, s);
+        int len = sb.length();
+        sb.append(root.val);
+        if(root.left == null && root.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(res, root.left, sb);
+            helper(res, root.right, sb);
+        }
+        sb.setLength(len);
     }
 }
+// class Solution {
+//     Set<String> l = new HashSet<>();
+//     public List<String> binaryTreePaths(TreeNode root) {
+//         rec(root, "");
+//         return new LinkedList<String>(l); 
+//     }
+//     public void rec(TreeNode root, String s){
+//         if(root == null)
+//             return;
+//         if(root.left == null && root.right == null){
+//             l.add(s+Integer.toString(root.val));
+//             return;
+//         }
+//         s += Integer.toString(root.val) + "->";
+//         rec(root.left, s);
+//         rec(root.right, s);
+//     }
+// }
