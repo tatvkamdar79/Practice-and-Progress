1fc997de40032b9d6300874a3c04ea6058234185
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-06-16 13:02:08 +0530

    Time: 13 ms (54.63%), Space: 42.9 MB (51.69%) - LeetHub

--- 401-binary-watch/401-binary-watch.java
+++ 401-binary-watch/401-binary-watch.java
@@ -1,9 +1,10 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
+        int[] bc = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5};
         List<String> ans = new LinkedList<>();
         for(int i = 0; i < 12; i++)
             for(int j = 0; j < 60; j++)
-                if(Integer.bitCount(i) + Integer.bitCount(j) == turnedOn)
+                if(bc[i] + bc[j] == turnedOn)
                     ans.add(Integer.toString(i) + ":" + (j >= 10 ? Integer.toString(j) : ("0" + Integer.toString(j))));
         return ans;
     }
