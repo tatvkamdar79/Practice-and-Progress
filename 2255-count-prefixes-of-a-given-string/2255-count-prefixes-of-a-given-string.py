a4fdb955d2760aa26c81099a530cd1cbf034d824
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-06-20 16:06:54 +0530

    Time: 113 ms (12.05%), Space: 14.1 MB (92.27%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        c = 0
+        for i in words:
+            if s.find(i) == 0:
+                c += 1
+        return c
