9c59d23a29b8ba6694a3f308ce215cdcfe08f8a7
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-04 01:21:41 +0530

    Time: 0 ms (100.00%), Space: 12.3 MB (91.60%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,50 +9,47 @@
  */
 class Solution {
 public:
-    vector<int> graph[500];
-    void makeadjlist(TreeNode* root){
-        if(root->left){
-            graph[root->val].push_back(root->left->val);
-            graph[root->left->val].push_back(root->val);
-            makeadjlist(root->left);
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    }
+    vector<int> ans;
+    int dfs(TreeNode* node, TreeNode* target, int k) {
+        if(node == NULL)
+            return -1;
+        if(node == target) {
+            subtreeAdd(node, k, 0);
+            return 1;
         }
-        if(root->right){
-            graph[root->val].push_back(root->right->val);
-            graph[root->right->val].push_back(root->val);
-            makeadjlist(root->right);
+        int left = dfs(node->left, target, k), right = dfs(node->right, target, k);
+        if(left == k) {
+            ans.push_back(node->val);
+            return left+1;
+        }else if(left < k && left != -1) {
+            subtreeAdd(node->right, k, left+1);
+            return left+1;
+        }else if(right == k) {
+            ans.push_back(node->val);
+            return right+1;
+        }else if(right < k && right != -1) {
+            subtreeAdd(node->left, k, right+1);
+            return right+1;
+        }else {
+            return -1;
+        }
+    }
+    void subtreeAdd(TreeNode* node, int k, int dist) {
+        if(node == NULL)
+            return;        
+        if(dist == k)
+            ans.push_back(node->val);
+        else {
+            subtreeAdd(node->left, k, dist+1);
+            subtreeAdd(node->right, k, dist+1);
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if(k == 0){
-            return {target->val};
-        }
-        queue<int> q;
-        q.push(target->val);
-        makeadjlist(root);
-        int curr_dist = 0;
-        vector<bool> vis(500,0);
-        while(!q.empty()){
-            int s = q.size();
-            while(s--){
-                int curr = q.front();
-                q.pop();
-                vis[curr] = 1;
-                for(int neigbhour : graph[curr]){
-                    if(!vis[neigbhour]){
-                        q.push(neigbhour);
-                    }
-                }
-            }
-            curr_dist++;
-            if(curr_dist == k){
-                break;
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            ans.push_back(q.front());
-            q.pop();
-        }
+        dfs(root, target, k);
         return ans;
     }
 };
