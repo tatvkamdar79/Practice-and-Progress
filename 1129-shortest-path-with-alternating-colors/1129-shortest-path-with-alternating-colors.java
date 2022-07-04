337cc4adb974499c78ae534ae317cb53b2581fbd
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-05 00:02:21 +0530

    Time: 2 ms (99.85%), Space: 46.7 MB (81.04%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,66 +1,39 @@
 class Solution {
-    public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        int[][] g = new int[n][n];
-        buildGraph(g, n, red_edges, blue_edges);
-        
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 1});
-        q.offer(new int[]{0, -1});
-        int len = 0;
-        int[] res = new int[n];
-        Arrays.fill(res, Integer.MAX_VALUE);
-        res[0] = 0;
-        
-        Set<String> visited = new HashSet<>();
-        while (!q.isEmpty()) {
-            int size = q.size();
-            len++;
-            for (int i = 0; i < size; i++) {
-                int[] cur = q.poll();
-                int node = cur[0];
-                int color = cur[1];
-                int oppoColor = -color;
-                
-                for (int j = 1; j < n; j++) {
-                    if (g[node][j] == oppoColor ||
-                       g[node][j] == 0) {
-                        if (!visited.add(j + "" + oppoColor)) continue;
-                        q.offer(new int[]{j, oppoColor});
-                        res[j] = Math.min(res[j], len);
-                    }
-                }
-            }
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+        int[] redLastPaths = new int[n];
+        int[] blueLastPaths = new int[n];
+        Arrays.fill(redLastPaths, -1);
+        Arrays.fill(blueLastPaths, -1);
+        redLastPaths[0] = blueLastPaths[0] = 0;
+        while (true)
+            if (exploreEdges(redEdges, redLastPaths, blueLastPaths) && // from blue last to red last
+                    exploreEdges(blueEdges, blueLastPaths, redLastPaths)) // from red last to blue last
+                break;
+        for (int p = 0; p < n; p++) {
+            if (redLastPaths[p] == -1)
+                redLastPaths[p] = blueLastPaths[p];
+            else if (blueLastPaths[p] == -1)
+                redLastPaths[p] = redLastPaths[p];
+            else
+                redLastPaths[p] = Math.min(redLastPaths[p], blueLastPaths[p]);
         }
-        
-        for (int i = 1; i < n; i++) {
-            if (res[i] == Integer.MAX_VALUE) {
-                res[i] = -1;
-            }
-        }
-        
-        return res;
+        return redLastPaths;
     }
     
-    private void buildGraph(int[][] g, int n, int[][] red_edges, int[][] blue_edges) {
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(g[i], -n);
-        }
-        
-        for (int[] e : red_edges) {
-            int from = e[0];
-            int to = e[1];
-            g[from][to] = 1;
-        }
-        
-        for (int[] e : blue_edges) {
-            int from = e[0];
-            int to = e[1];
-            if (g[from][to] == 1) {
-                g[from][to] = 0;
-            } else {
-                g[from][to] = -1;
+    // Updates paths with one layer of exploration via edges.
+    // Returns whether a change was made to paths.
+    private boolean exploreEdges(int[][] edges, int[] fromPaths, int[] toPaths) {
+        boolean done = true;
+        int[] prevToPaths = toPaths.clone();
+        for (int[] edge : edges) {
+            // if first valid path to edge[1]
+            if (fromPaths[edge[0]] != -1 &&
+                    (toPaths[edge[1]] == -1 || fromPaths[edge[0]] + 1 < toPaths[edge[1]])) {
+                toPaths[edge[1]] = fromPaths[edge[0]] + 1;
+                done = false;
             }
         }
+        return done;
     }
 }
 // class Solution {
