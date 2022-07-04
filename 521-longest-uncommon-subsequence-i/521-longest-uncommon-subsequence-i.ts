29fdf17c39cdb64f3e2a127d00e7cff34bfdad84
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-05 01:01:22 +0530

    Time: 93 ms (46.88%), Space: 43.2 MB (25.00%) - LeetHub

---
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.ts
@@ -0,0 +1,10 @@
+function findLUSlength(a: string, b: string): number {
+    let answer: number = -1 
+
+    if (a !== b){
+        answer = Math.max(a.length, b.length)
+        return answer
+    }
+    
+    return answer 
+};
