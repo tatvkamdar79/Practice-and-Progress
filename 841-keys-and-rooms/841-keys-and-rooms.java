5db51b9390eedadf9ee9e8c3c699d49028363730
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-10 17:05:21 +0530

    Time: 2 ms (75.57%), Space: 45 MB (27.69%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -4,20 +4,19 @@ class Solution {
         seen[0] = true;
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
-
-        //At the beginning, we have a todo list "stack" of keys to use.
-        //'seen' represents at some point we have entered this room.
-        while (!q.isEmpty()) { // While we have keys...
-            int node = q.poll(); // Get the next key 'node'
-            for (int nei: rooms.get(node)) // For every key in room # 'node'...
-                if (!seen[nei]) { // ...that hasn't been used yet
-                    seen[nei] = true; // mark that we've entered the room
-                    q.add(nei); // add the key to the todo list
+        
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for(int nei: rooms.get(node))
+                if(!seen[nei]){
+                    seen[nei] = true;
+                    q.add(nei);
                 }
         }
-
-        for (boolean v: seen)  // if any room hasn't been visited, return false
-            if (!v) return false;
+        
+        for(boolean v: seen)
+            if(!v)
+                return false;
         return true;
     }
 }
