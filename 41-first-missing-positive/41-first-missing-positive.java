026e50e91af417f83617052380b694d3789b3c88
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-14 13:36:36 +0530

    Time: 4 ms (70.57%), Space: 128.3 MB (28.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,28 +1,19 @@
 class Solution {
+    public void swap(int a, int b, int[] nums){
+        int x = nums[a];
+        nums[a] = nums[b];
+        nums[b] = x;
+    }
     public int firstMissingPositive(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < 0) nums[i] = 0;
+        int n = nums.length, i = 0;
+        while(i < n){
+            if(nums[i] == i + 1 || nums[i] <= 0 || nums[i] > n) i++;
+            else if(nums[nums[i] - 1] != nums[i])   swap(nums[i] - 1, i, nums);
+            else    i++;
         }
-        
-        for(int i = 0; i < nums.length; i++){
-            int val = Math.abs(nums[i]);
-            if(val > 0 && val <= nums.length){
-                if(nums[val - 1] == 0)    nums[val - 1] = - (nums.length + 1);
-                else    nums[val - 1] = - Math.abs(nums[val - 1]);
-            }
-        }
-        
-        for(int i = 1; i < nums.length + 1; i++){
-            if(nums[i-1] >= 0)   return i;
-        }
-        
-        return nums.length + 1;
-        
-        
-        
-        
-        
-        
+        i = 0;
+        while(i < nums.length && nums[i] == i+1) i++;
+        return i+1;
         
         // int[] x = new int[nums.length + 1];
         // for(int i = 0; i < nums.length; i++){
