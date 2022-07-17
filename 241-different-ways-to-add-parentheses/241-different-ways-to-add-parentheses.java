bed38b5d54301751b3f930daa3868ce041c5d995
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-07-17 12:40:54 +0530

    Time: 8 ms (9.66%), Space: 43 MB (51.97%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -1,33 +1,39 @@
 public class Solution {
+    
+    Map<String, List<Integer>> map = new HashMap<>();
+    
     public List<Integer> diffWaysToCompute(String input) {
-        List<Integer> ret = new LinkedList<Integer>();
-        for (int i=0; i<input.length(); i++) {
-            if (input.charAt(i) == '-' ||
-                input.charAt(i) == '*' ||
-                input.charAt(i) == '+' ) {
-                String part1 = input.substring(0, i);
-                String part2 = input.substring(i+1);
-                List<Integer> part1Ret = diffWaysToCompute(part1);
-                List<Integer> part2Ret = diffWaysToCompute(part2);
-                for (Integer p1 :   part1Ret) {
-                    for (Integer p2 :   part2Ret) {
-                        int c = 0;
-                        switch (input.charAt(i)) {
-                            case '+': c = p1+p2;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                String p1 = input.substring(0, i);
+                String p2 = input.substring(i + 1);
+                List<Integer> l1 = map.getOrDefault(p1, diffWaysToCompute(p1));
+                List<Integer> l2 = map.getOrDefault(p2, diffWaysToCompute(p2));
+                for (Integer i1 : l1) {
+                    for (Integer i2 : l2) {
+                        int r = 0;
+                        switch (c) {
+                            case '+':
+                                r = i1 + i2;
                                 break;
-                            case '-': c = p1-p2;
+                            case '-':
+                                r = i1 - i2;
                                 break;
-                            case '*': c = p1*p2;
+                            case '*':
+                                r = i1 * i2;
                                 break;
                         }
-                        ret.add(c);
+                        res.add(r);
                     }
                 }
             }
         }
-        if (ret.size() == 0) {
-            ret.add(Integer.valueOf(input));
+        if (res.size() == 0) {
+            res.add(Integer.valueOf(input));
         }
-        return ret;
+        map.put(input, res);
+        return res;
     }
 }
