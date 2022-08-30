49028453751202f8e0bbc4497aec53acbd3decd8
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-08-30 20:28:18 +0530

    Time: 25 ms (95.47%), Space: 93.9 MB (58.05%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -1,26 +1,16 @@
-class Solution:
-    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        g = p = m = 0
-        for i in range(len(garbage) - 1, -1, -1):
-            if g > 0 and p > 0 and m > 0:
-                break
-            if g == 0 and 'G' in garbage[i]:
-                g = i
-            if p == 0 and 'P' in garbage[i]:
-                p = i
-            if m == 0 and 'M' in garbage[i]:
-                m = i
-        
-        # print(g, p, m)
-        ans = 0
-        for i in range(g + 1):
-            ans += garbage[i].count('G')
-            ans += travel[i] if i != g else 0
-        for i in range(p + 1):
-            ans += garbage[i].count('P')
-            ans += travel[i] if i != p else 0
-        for i in range(m + 1):
-            ans += garbage[i].count('M')
-            ans += travel[i] if i != m else 0
-            
-        return ans
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int last[] = new int[128], res = 0;
+        for (int i = 0; i < garbage.length; ++i) {
+            res += garbage[i].length();
+            for (int j = 0; j < garbage[i].length(); ++j)
+                last[garbage[i].charAt(j)] = i;
+        }
+        for (int j = 1; j < travel.length; ++j)
+            travel[j] += travel[j - 1];
+        for (int c : "PGM".toCharArray())
+            if (last[c] > 0)
+                res += travel[last[c] - 1];
+        return res;
+    }
+}
