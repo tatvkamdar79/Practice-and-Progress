5469070a6dcf2d928251f02cc22c62d4be17e42a
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-09-05 09:05:38 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (18.18%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
+        // https://mathfour.com/arithmetic/can-every-number-be-written-as-a-palindrome-in-some-base
         return false;
     }
 }
