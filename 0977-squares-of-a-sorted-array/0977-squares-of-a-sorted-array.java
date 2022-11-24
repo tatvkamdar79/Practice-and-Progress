89e8775a1c5db872459dc24acadb166c00a1d246
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-11-24 16:13:10 +0530

    Time: 2 ms (89.89%), Space: 55.8 MB (31.38%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0; i < nums.length; i++)
-            nums[i] = Math.abs(nums[i]);
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++)
-            nums[i] *= nums[i];
-        return nums;
-        // int n = nums.length;
-        // int[] ans = new int[n];
-        // int i = 0, j = n - 1;
-        // for(int idx = n - 1; idx >= 0; idx--){
-        //     if(Math.abs(nums[i]) > Math.abs(nums[j]))
-        //         ans[idx] = nums[i] * nums[i++];
-        //     else
-        //         ans[idx] = nums[j] * nums[j--];
-        // }
-        // return ans;
+        // N A I V E   S O L U T I O N (SORT + SQUARE)
+        // for(int i = 0; i < nums.length; i++)
+        //     nums[i] = Math.abs(nums[i]);
+        // Arrays.sort(nums);
+        // for(int i = 0; i < nums.length; i++)
+        //     nums[i] *= nums[i];
+        // return nums;
+        
+        // S M A R T    S O L U T I O N
+        int n = nums.length;
+        int[] ans = new int[n];
+        int i = 0, j = n - 1;
+        for(int idx = n - 1; idx >= 0; idx--){
+            if(Math.abs(nums[i]) > Math.abs(nums[j]))
+                ans[idx] = nums[i] * nums[i++];
+            else
+                ans[idx] = nums[j] * nums[j--];
+        }
+        return ans;
     }
 }
