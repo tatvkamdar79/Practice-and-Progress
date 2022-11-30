48abf6198138f8c3482461510d3dd06c80420a36
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-11-30 13:26:14 +0530

    Time: 85 ms (11.08%), Space: 14.2 MB (5.18%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -2,4 +2,5 @@ from collections import Counter as ctr
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         c = ctr(arr)
-        return len(c.values()) == len(set(c.values()))
+        return len(set(c.values())) == len(set(c.keys()))
+        # return len(c.values()) == len(set(c.values()))
