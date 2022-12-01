39b792c6255d4ec51077b300b7f652ce73542bad
tatvkamdar79 <68917370+tatvkamdar79@users.noreply.github.com>
2022-12-01 22:31:24 +0530

    Time: 88 ms (26.11%), Space: 72.3 MB (9.35%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        int n = nums.length;
-        List<Boolean> ans = new ArrayList<>();
-        for(int q = 0; q < l.length; q++){
-            int len = r[q] - l[q] + 1;
-            int[] arr = new int[len];
-            for(int i = 0; i < len; i++){
-                arr[i] = nums[l[q]+i];
-            }
-            Arrays.sort(arr);
-            boolean isAP = true;
-            for(int i = 1; i < len - 1; i++){
-                if(arr[i] - arr[i-1] != arr[i+1] - arr[i]){
-                    isAP = false;
-                    break;
-                }
-            }
-            ans.add(isAP);
+        List<Boolean> res = new ArrayList<Boolean>();
+        for(int i = 0; i < l.length; i++) {
+            res.add(isArithmeticSeq(nums, l[i], r[i]));
         }
-        return ans;
+        
+        return res;
+    }
+    
+    private boolean isArithmeticSeq(int[] nums, int start, int end) {
+        if(end - start < 2) return true;
+        
+        int min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = start; i <= end; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+            set.add(nums[i]);
+        }
+        
+        if((max - min) % (end - start) != 0) return false;
+        int interval = (max - min) / (end -start);
+        
+        for(int i = 1; i <= end -start; i++) {
+            if(!set.contains(min + i * interval)) return false;
+        }
+        
+        return true;
     }
 }
